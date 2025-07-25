51d555ad52a0ffca5d60b1c525ab89e714b8d979
alain-ateh <mrmeloateh@yahoo.com>
2025-07-25 05:54:28 -0400

    prod final commit

---
+++ prod/version.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.64.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.7.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.22.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
