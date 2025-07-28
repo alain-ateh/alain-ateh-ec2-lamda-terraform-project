f352d55c2cf765b25d5616efd720433b08a6325f
alain-ateh <mrmeloateh@yahoo.com>
2025-07-27 20:36:56 -0400

    ec2 output commit

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -15,24 +15,46 @@ output "security_group_arn" {
 
 output "instance_id" {
   description = "ID of the Jenkins EC2 instance"
-  value       = aws_instance.jenkins_server.id
+  value       = aws_instance.jenkins_server.id  # ← Fixed: jenkins_server instead of ec2_instance
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the Jenkins EC2 instance"
-  value       = aws_instance.jenkins_server.public_ip
+  value       = aws_instance.jenkins_server.public_ip  # ← Fixed
 }
 
 output "instance_private_ip" {
   description = "Private IP address of the Jenkins EC2 instance"
-  value       = aws_instance.jenkins_server.private_ip
+  value       = aws_instance.jenkins_server.private_ip  # ← Fixed
 }
 
 output "instance_public_dns" {
   description = "Public DNS name of the Jenkins EC2 instance"
-  value       = aws_instance.jenkins_server.public_dns
+  value       = aws_instance.jenkins_server.public_dns  # ← Fixed
 }
+
 output "instance_private_dns" {
   description = "Private DNS name of the Jenkins EC2 instance"
-  value       = aws_instance.jenkins_server.private_dns
+  value       = aws_instance.jenkins_server.private_dns  # ← Fixed
+}
+output "instance_tags" {
+  description = "Tags associated with the Jenkins EC2 instance"
+  value       = aws_instance.jenkins_server.tags
+}
+
+output "instance_name" {
+  description = "Name tag of the Jenkins EC2 instance"
+  value       = aws_instance.jenkins_server.tags["Name"]  # ← Fixed
+}
+output "instance_environment" {
+  description = "Environment tag of the Jenkins EC2 instance"
+  value       = aws_instance.jenkins_server.tags["Environment"]  # ← Fixed
+}
+output "instance_type" {
+  description = "Type of the Jenkins EC2 instance"
+  value       = aws_instance.jenkins_server.instance_type  # ← Fixed
+}
+output "instance_ami" {
+  description = "AMI ID of the Jenkins EC2 instance"
+  value       = aws_instance.jenkins_server.ami  # ← Fixed
 }
